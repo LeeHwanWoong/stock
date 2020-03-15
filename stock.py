66f8f25a89daaa49eb342d1b0808d0417d507c08
root <lhw3110@naver.com>
2020-03-15 17:53:09 +0900

    20200315

--- stock.py
+++ stock.py
@@ -1,11 +1,22 @@
+#-*- coding: utf-8 -*-
 import pandas as pd
 import pandas_datareader as pdr
 from datetime import datetime
+import openpyxl
 
 def get_code(df, name):
     code = df.query("name=='{}'".format(name))['code'].to_string(index=False)
 
     return code.strip()
+
+def get_name(df, code):
+    name = df.query("code=='{}'".format(code))['name'].to_string(index=False)
+
+    return name
+
+f = open("kosdaq_code.txt", 'r')
+codes = f.read().split()
+f.close()
 
 code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download',header=0)[0]
 
@@ -15,11 +26,10 @@ code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
 code_df.code = code_df.code.map('{:06d}'.format)
 
-code = get_code(code_df, '삼성전자')
-
-code = code+'.KS'
-
-df = pdr.get_data_yahoo(code, datetime(2020,1,1), datetime(2020,3,11))
-
-print(type(df))
-print(df)
+for code in codes:
+    name=get_name(code_df, code)
+    try:
+        data = pdr.get_data_yahoo(code+'.kq', datetime(2020,3,13), datetime(2020,3,13))
+        print(name+","+code+","+str(data.values[0][3]))
+    except Exception as ex:
+        pass
